c8d040ca84b0463f14f989e64b5160308548ad0c
ujjval <ujjval000@gmail.com>
2024-07-18 07:51:01 +0530

    chnge

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -32,7 +32,7 @@ a{
     <div class="container">
   <div class="row">
     <div class="col-md-6 offset-md-3">
-      <h2 class="text-center text-dark mt-5">Login Form</h2>
+      <h2 class="text-center text-dark mt-5">Login Form  ujjval</h2>
       <div class="text-center mb-5 text-dark">Made with bootstrap</div>
       <div class="card my-5">
 
